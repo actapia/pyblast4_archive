f496102dc69365bfbdb0cff9b852be01af4742e7
actapia <andrew.tapia@uky.edu>
2026-01-31 00:04:51 -0500

    Make LICENSES files in release workflow.

--- prepare_dep_linux.sh
+++ prepare_dep_linux.sh
@@ -5,11 +5,15 @@ rpmbuild_dir="$HOME/rpmbuild"
 mkdir -p deps
 rm -rf "$rpmbuild_dir"
 tmp="$(mktemp -d)"
-yumdownloader --source "$1" --dest "$tmp"
+yumdownloader "$1" --dest "$tmp" >&2
+original_name="$(rpm -q "$tmp"/*.rpm --qf '%{NAME}\n' | head -n1)"
+yumdownloader --source "$1" --dest "$tmp" >&2
 spec="$(rpm -q -l "$tmp"/*.src.rpm | grep '\.spec$')"
 name="$(rpm -q "$tmp"/*.src.rpm --qf '%{NAME}\n')"
-rpm --install "$tmp"/*.src.rpm
-rpmbuild -bp --nodeps "$rpmbuild_dir/SPECS/$spec"
+version="$(rpm -q "$tmp"/*.src.rpm --qf '%{VERSION}\n')"
+license="$(rpm -q "$tmp"/*.src.rpm --qf '%{LICENSE}\n')"
+rpm --install "$tmp"/*.src.rpm >&2
+rpmbuild -bp --nodeps "$rpmbuild_dir/SPECS/$spec" >&2
 # build_dirs=(/root/rpmbuild/BUILD/*)
 # if [ "${#build_dirs[@]}" -eq 1 ]; then
 #     mv "${build_dirs[0]}" "deps/$name"
@@ -20,6 +24,12 @@ mv "$rpmbuild_dir/SPECS/$spec" "deps/$name/"
 mv "$rpmbuild_dir/SOURCES/" "deps/$name/patches"
 shopt -s nullglob
 rm "deps/$name/patches/"*.zip "deps/$name/patches/"*.tar*
+rm -rf "$tmp"
+echo -n "$name "
+if [ "$name" != "$original_name" ]; then
+    echo -n "($original_name) "
+fi
+echo "$version: $license"
 # mkdir "deps/$name"
 # mv /root/rpmbuild/SPECS/* "deps/$name/"
 # mv /root/rpmbuild/SOURCES/* "deps/$name/"
--- prepare_dep_macos.sh
+++ prepare_dep_macos.sh
@@ -2,5 +2,8 @@
 set -e
 set -x
 mkdir -p deps/"$1"
-brew get-source --out deps/"$1" "$1"
+brew get-source --out deps/"$1" "$1" >&2
 cp "$(brew edit --print-path "$1")" deps/"$1"/
+license="$(brew info --json "$1" | jq -r '.[0] | .license')"
+version="$(brew info "$1" | jq -r '.[0] | .versions.stable')"
+echo "$1 $version: $license"
